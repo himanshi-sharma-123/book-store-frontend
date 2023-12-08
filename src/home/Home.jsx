498c5f88deb7c154ca906217ef0a9c9cce7f2703
Himanshi Sharma <himanshish456@gmail.com>
2023-12-08 11:11:21 +0530

    banner component added

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return (
-    <div className="">
-      <div className="h-screen">Home</div>
-      <div className="h-screen bg-red-600"></div>
+    <div>
+      <Banner />
     </div>
   );
 };
 
 export default Home;
+
+// <div className="">
+//   <div className="h-screen">Home</div>
+//   <div className="h-screen bg-red-600"></div>
+// </div>
