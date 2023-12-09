f4d624bd110ac3040694d946fab940e667df3afb
Himanshi Sharma <himanshish456@gmail.com>
2023-12-09 17:41:40 +0530

    dashboard routes

---
+++ src/dashboard/EditBooks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditBooks = () => {
+  return (
+    <div>
+      <h1>edit books</h1>
+    </div>
+  );
+};
+
+export default EditBooks;
---
+++ src/dashboard/UploadBook.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UploadBook = () => {
+  return (
+    <div>
+      <h1>upload book</h1>
+    </div>
+  );
+};
+
+export default UploadBook;
