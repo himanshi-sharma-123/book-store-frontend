7905855e438f8bfd1e2a511657c0b27fa5be1a21
Himanshi Sharma <himanshish456@gmail.com>
2024-01-27 17:45:12 +0530

    upgrade

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0Q2MnqjmVgZS--uqQooT_4YEHh6IT8Kk",
-  authDomain: "book-store-104e9.firebaseapp.com",
-  projectId: "book-store-104e9",
-  storageBucket: "book-store-104e9.appspot.com",
-  messagingSenderId: "101424806811",
-  appId: "1:101424806811:web:bb64197ab099a37f3e3f3e",
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_APP_ID,
 };
 
 // Initialize Firebase
